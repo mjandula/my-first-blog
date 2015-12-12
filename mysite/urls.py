51950baafb8d6b5141799c9f98b65a1218f7419e
montse <mjandula@gmail.com>
2015-12-12 16:51:26 +0100

    static

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'', include('blog.urls')),
-]
+
+] 
